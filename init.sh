0cc1c6366937ad56180c6dd9a7843b4530f8817b
Stevel <sasha30.07.98@gmail.com>
2019-08-30 16:05:23 +0200

    conf

--- init.sh
+++ init.sh
@@ -3,3 +3,9 @@
 # sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
+
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+# sudo ln -s /home/snowy_owl/git/Stepik/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+# sudo systemctl restart nginx.service
