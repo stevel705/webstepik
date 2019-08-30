122c609c158ea5253e7a99ae07d10bd38c3fce88
Stevel <sasha30.07.98@gmail.com>
2019-08-30 16:21:58 +0200

    conf

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@
 
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-# sudo ln -s /home/snowy_owl/git/Stepik/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# sudo ln -s /home/webstepik/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 # sudo systemctl restart nginx.service
