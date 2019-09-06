1279cda03c11e5fb69d3f342124b5d92903b9807
Stevel <sasha30.07.98@gmail.com>
2019-09-06 15:44:03 +0200

    add hello.py and add in init

--- init.sh
+++ init.sh
@@ -6,6 +6,9 @@
 
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-# sudo ln -s /home/webstepik/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
+# sudo ln -s /home/webstepik/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 # sudo systemctl restart nginx.service
+# sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+# sudo /etc/init.d/gunicorn restart
