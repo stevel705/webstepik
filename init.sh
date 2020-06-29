d3ff924ae71dc4cc18e7f1cb8d2c6b4c2291601d
Alexandr <stevel705@outlook.com>
2020-06-29 11:55:47 +0000

    add proxi in nginx.conf for gunicorn

--- init.sh
+++ init.sh
@@ -5,9 +5,12 @@
 # sudo /etc/init.d/mysql start
 
 sudo unlink /etc/nginx/sites-enabled/default
+# sudo rm /etc/nginx/sites-enabled/default
+
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
+
 # sudo ln -s /home/webstepik/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 # sudo systemctl restart nginx.service
 # sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
