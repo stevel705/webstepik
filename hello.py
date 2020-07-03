2e53e064f5b6247ba293eeb37daa414b4e3039a0
Alexandr <stevel705@outlook.com>
2020-07-03 08:56:35 +0000

    nginx.conf

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@ def wsgi_application(environ, start_response):
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return '\n'.join(environ['QUERY_STRING'].split('&'))
+
+
+
+# gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
--- init.sh
+++ init.sh
@@ -4,12 +4,12 @@
 # sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
 
-sudo unlink /etc/nginx/sites-enabled/default
+# sudo unlink /etc/nginx/sites-enabled/default
 # sudo rm /etc/nginx/sites-enabled/default
 
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+# sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 # sudo ln -s /home/webstepik/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 # sudo systemctl restart nginx.service
