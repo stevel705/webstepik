455bf059bbee786d7fe7a152bd7be9f69bd5e7b2
Alexandr Meshkov <sasha30.07.98@gmail.com>
2020-07-03 12:42:44 +0000

    add ask urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 from . import views 
 
-urlpatterns = patterns('qa.views',                                              
-   url(r'^$', 'test'),                                                              
-   url(r'^login/.*$', 'test', name='login'),                                    
-   url(r'^signup/.*', 'test', name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
-   url(r'^ask/.*', 'test', name='ask'),                                         
-   url(r'^popular/.*', 'test', name='popular'),                                 
-   url(r'^new/.*', 'test', name='new'),                                         
-)    
+urlpatterns = [                                              
+   url(r'^$', 'views.test'),                                                              
+   url(r'^login/.*$', 'views.test', name='login'),                                    
+   url(r'^signup/.*', 'views.test', name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', 'views.test', name='question'),                 
+   url(r'^ask/.*', 'views.test', name='ask'),                                         
+   url(r'^popular/.*', 'views.test', name='popular'),                                 
+   url(r'^new/.*', 'views.test', name='new'),                                         
+]
 
 
