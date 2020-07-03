30b81cbfc4fa31123b9356c63ee5ed6d3ec0f2f5
Alexandr <stevel705@outlook.com>
2020-07-03 09:27:13 +0000

    solution IncompleteRead(0 bytes read)

--- hello.py
+++ hello.py
@@ -2,8 +2,11 @@ def wsgi_application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return '\n'.join(environ['QUERY_STRING'].split('&'))
+    # return '\n'.join(environ['QUERY_STRING'].split('&'))
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    return body
 
 
 
 # gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
+
